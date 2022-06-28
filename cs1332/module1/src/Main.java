4dda34065e42fa22028f4072d315c20285321891
George Vasilchenko <george.vasilchenko@hotmail.com>
2022-06-28 22:32:18 +0200

    Add module1

---
+++ cs1332/module1/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Customer> list = new ArrayList<>(100);
+        System.out.println("Hello world!");
+    }
+}
